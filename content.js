de5717a35d6eaebc546dfba76f59b43872c98a5d
Alex Yu <peihaoy@gmail.com>
2017-01-03 20:56:23 -0500

    Update content.js

--- content.js
+++ content.js
@@ -5,67 +5,56 @@ document.body.appendChild(bubbleDOM);
 
 // Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
-  var selection = window.getSelection().toString();
-  if (selection.length > 0) {
-	  searchText(e.clientX, e.clientY, selection);
-  }
+	var selection = window.getSelection().toString();
+  	if (selection.length > 0) {
+		searchText(e.clientX, e.clientY, selection);
+  	}
 }, false);
 
 
 // Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
-  bubbleDOM.style.visibility = 'hidden';
+  	bubbleDOM.style.visibility = 'hidden';
 }, false);
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
-  bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
+  	bubbleDOM.innerHTML = selection;
+  	bubbleDOM.style.top = mouseY + 'px';
+  	bubbleDOM.style.left = mouseX + 'px';
+  	bubbleDOM.style.visibility = 'visible';
 }
 
 
 function searchText(X,Y,word) {	
-    //var newURL = "https://www.merriam-webster.com/dictionary/"+word;
-    //chrome.tabs.create({ url: newURL });
-    request = new XMLHttpRequest();
+    	//var newURL = "https://www.merriam-webster.com/dictionary/"+word;
+    	//chrome.tabs.create({ url: newURL });
+    	request = new XMLHttpRequest();
     
     
-    request.open("GET", "http://www.dictionaryapi.com/api/v1/references/learners/xml/"+word+"?key=[KEY HERE]", true);
-    request.send();
-    request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == 200) {
-        //console.log(request.responseXML);
-		var returned_xml = request.responseText;
-		var start = returned_xml.indexOf("<dt>");   //"dt>:");
-		//var textin = start.toString()+" kk";
-		var end = 0;
-		if(returned_xml.indexOf("<wsgram")>returned_xml.indexOf("<vi")|| returned_xml.indexOf("<wsgram") == -1){
-			end = returned_xml.indexOf("<vi");
+    	request.open("GET", "http://www.dictionaryapi.com/api/v1/references/learners/xml/"+word+"?key=[API KEY]", true);
+    	request.send();
+    	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+
+			//parse xml returned
+			var returned_xml = request.responseText;		
+			var front = returned_xml.substring(returned_xml.indexOf(":")+1);
+			var fin_string = front.substring(0, front.indexOf("<"));
+
+
+			renderBubble(X, Y, fin_string);
+
+
+		} else if (request.readyState == 4 && request.status != 200) {
+			//error in connection
+			renderBubble(X, Y, "Connection Error");
+		} else {
+			//loading issues?
+			renderBubble(X, Y, "Loading?");
+
 		}
-		else{
-			end = returned_xml.indexOf("<wsgram");
-		}
-		var fin_string = returned_xml.slice(start, end);
-	
-		//chrome.tabs.create({ url: typeof(returned_xml)});
-		//chrome.tabs.create({ url: fin_string});
-		//chrome.tabs.create({ url: request.responseText});
-		//chrome.tabs.create({ url: textin});
-		
-		renderBubble(X, Y, fin_string);
-		
-		
-      } else if (request.readyState == 4 && request.status != 200) {
-        //console.log("not found");
-		renderBubble(X, Y, "Connection Error");
-      } else {
-		//console.log("not found");
-		renderBubble(X, Y, "Loading?");
-        //searchText(X,Y,word);
-      }
-    }
+    	}
 
 }
 
@@ -85,3 +74,4 @@ function searchText(X,Y,word) {
 });
 
 **/
+
